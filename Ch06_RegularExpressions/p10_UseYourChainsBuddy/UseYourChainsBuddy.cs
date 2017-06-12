77d6195dcf96548c0fcb48c3d368cdf68f74d861
Pilgrimage <nanobot149@gmail.com>
2017-06-12 13:04:15 +0300

    Done.

--- Ch06_RegularExpressions/p10_UseYourChainsBuddy/UseYourChainsBuddy.cs
+++ Ch06_RegularExpressions/p10_UseYourChainsBuddy/UseYourChainsBuddy.cs
@@ -2,19 +2,50 @@
 {
     using System;
     using System.Text.RegularExpressions;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using System.Threading.Tasks;
 
     public class UseYourChainsBuddy
     {
         public static void Main()
         {
+            char[] acceptable = "abcdefghijklmnopqrstuvwxyz1234567890".ToCharArray();
+            string inputText = Console.ReadLine();
 
+            string patternTags = @"<p>(.*?)<\/p>";
+            string result = "";
 
+            MatchCollection inputTags = Regex.Matches(inputText, patternTags);
 
+            foreach (Match inputTag in inputTags)
+            {
+                char[] tagText = inputTag.Groups[1].Value.ToCharArray();
 
+                for (int i = 0; i < tagText.Length; i++)
+                {
+                    if (!acceptable.Contains(tagText[i]))
+                    {
+                        tagText[i] = ' ';
+                    }
+                    else if (tagText[i] >= 'a' && tagText[i] <= 'z')
+                    {
+
+                        if (tagText[i] <= 'm')
+                        {
+                            tagText[i] = (char)(tagText[i] + 13);
+                        }
+                        else
+                        {
+                            tagText[i] = (char)(tagText[i] - 13);
+                        }
+                    }
+
+                }
+                result += String.Join("", tagText);
+            }
+
+            result = Regex.Replace(result, "[ ]{2,}", " ");
+            Console.WriteLine(result);
         }
     }
 }
---
+++ Ch06_RegularExpressions/p10_UseYourChainsBuddy2/UseYourChainsBuddy.cs
@@ -0,0 +1,42 @@
+﻿namespace p10_UseYourChainsBuddy2
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class UseYourChainsBuddy
+    {
+        public static void Main()
+        {
+            string inputText = Console.ReadLine();
+
+            string patternTags = @"<p>(.*?)<\/p>";
+            string patternText = @"[^a-z0-9]+";
+            string preResult = "";
+
+            MatchCollection inputTags = Regex.Matches(inputText, patternTags);
+
+            foreach (Match inputTag in inputTags)
+            {
+                string tagContent = Regex.Replace(inputTag.Groups[1].Value, patternText, " ");
+                preResult += tagContent;
+            }
+            
+            char[] text = preResult.ToCharArray();
+            int len = text.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (text[i] >= 'a' && text[i] <= 'm')
+                {
+                    text[i] = (char)(text[i] + 13);
+                }
+                else if (text[i] >= 'n' && text[i] <= 'z')
+                {
+                    text[i] = (char)(text[i] - 13);
+                }
+            }
+
+            Console.WriteLine(string.Join("", text));
+        }
+    }
+}
