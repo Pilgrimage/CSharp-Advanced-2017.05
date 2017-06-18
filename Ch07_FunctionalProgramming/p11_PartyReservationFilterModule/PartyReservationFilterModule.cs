6593bc1976f271975234ca893ceae2202cbe69be
Pilgrimage <nanobot149@gmail.com>
2017-06-18 10:21:39 +0300

    Done.

--- Ch07_FunctionalProgramming/p11_PartyReservationFilterModule/PartyReservationFilterModule.cs
+++ Ch07_FunctionalProgramming/p11_PartyReservationFilterModule/PartyReservationFilterModule.cs
@@ -3,16 +3,117 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
 
     public class PartyReservationFilterModule
     {
+        private static List<string> partisans = new List<string>();
+        private static Dictionary<string, HashSet<string>> filters = new Dictionary<string, HashSet<string>>();
+
         public static void Main()
         {
-            // video exercise 00:22:00
+            partisans = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            filters = InitFilterList(filters);
+
+            string input = Console.ReadLine();
+
+            while (input != "Print")
+            {
+                string[] inputParts = input.Trim().Split(';');
+                string command = inputParts[0];
+                string filter = inputParts[1];
+                string arg = inputParts[2];
+
+                if (command == "Add filter")
+                {
+                    filters[filter].Add(arg);
+                }
+                else if (command == "Remove filter")
+                {
+                    if (filters[filter].Contains(arg))
+                    {
+                        filters[filter].Remove(arg);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            partisans = ApplyFilters(partisans);
+
+            Console.WriteLine(string.Join(" ", partisans));
+
+        }
 
 
+        private static Dictionary<string, HashSet<string>> InitFilterList(Dictionary<string, HashSet<string>> filters)
+        {
+            filters.Add("Starts with", new HashSet<string>());
+            filters.Add("Ends with", new HashSet<string>());
+            filters.Add("Length", new HashSet<string>());
+            filters.Add("Contains", new HashSet<string>());
+            return filters;
+        }
 
+        private static List<string> ApplyFilters(List<string> names)
+        {
+            List<string> result = new List<string>(names);
+            if (filters["Starts with"].Count > 0)
+            {
+                result = ApplyStartsWith(result);
+            }
+            if (filters["Ends with"].Count > 0)
+            {
+                result = ApplyEndssWith(result);
+            }
+            if (filters["Length"].Count > 0)
+            {
+                result = ApplyLength(result);
+            }
+            if (filters["Contains"].Count > 0)
+            {
+                result = ApplyContains(result);
+            }
+            return result;
+        }
+
+        private static List<string> ApplyStartsWith(List<string> names)
+        {
+            List<string> result = new List<string>(names);
+            foreach (string arg in filters["Starts with"])
+            {
+                result = result.Where(x => !x.StartsWith(arg)).ToList();
+            }
+            return result;
+        }
+
+        private static List<string> ApplyEndssWith(List<string> names)
+        {
+            List<string> result = new List<string>(names);
+            foreach (string arg in filters["Ends with"])
+            {
+                result = result.Where(x => !x.EndsWith(arg)).ToList();
+            }
+            return result;
+        }
+
+        private static List<string> ApplyLength(List<string> names)
+        {
+            List<string> result = new List<string>(names);
+            foreach (string arg in filters["Length"])
+            {
+                result = result.Where(x => x.Length != int.Parse(arg)).ToList();
+            }
+            return result;
+        }
+
+        private static List<string> ApplyContains(List<string> names)
+        {
+            List<string> result = new List<string>(names);
+            foreach (string arg in filters["Contains"])
+            {
+                result = result.Where(x => !x.Contains(arg)).ToList();
+            }
+            return result;
         }
     }
 }
--- Ch07_FunctionalProgramming/p12_InfernoIII/InfernoIII.cs
+++ Ch07_FunctionalProgramming/p12_InfernoIII/InfernoIII.cs
@@ -7,12 +7,117 @@
 
     public class InfernoIII
     {
+        private static List<string> filters = new List<string>();
+        private static HashSet<int> indexOfExtracted = new HashSet<int>();
+
         public static void Main()
         {
             // video exercise 00:25:00
 
+            int[] gems = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int gemsCount = gems.Length;
 
+            string input = Console.ReadLine();
 
+            while (input != "Forge")
+            {
+                string[] inputParts = input.Split(';');
+                string command = inputParts[0];
+                string filter = inputParts[1];
+                string arg = inputParts[2];
+
+                string filterFull = filter + ";" + arg;
+
+                if (command == "Exclude")
+                {
+                    filters.Add(filterFull);
+                }
+                else if (command == "Reverse")
+                {
+                    int index = filters.LastIndexOf(filterFull);
+                    filters.RemoveAt(index);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect command!");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (filters.Count > 0)
+            {
+                ApplyFilters(gems);
+            }
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < gemsCount; i++)
+            {
+                if (!indexOfExtracted.Contains(i))
+                {
+                    result.Add(gems[i]);
+                }
+            }
+
+            //result = gems.Where((x, index) => !indexOfExtracted.Contains(index)).ToList();
+
+            Console.WriteLine(string.Join(" ", result));
         }
+
+
+        private static void ApplyFilters(int[] gems)
+        {
+            foreach (string filter in filters)
+            {
+                ApplyFilter(gems, filter);
+            }
+        }
+
+        private static void ApplyFilter(int[] gems, string filerFull)
+        {
+            string[] filterSplited = filerFull.Split(';');
+            string filter = filterSplited[0];
+            int arg = int.Parse(filterSplited[1]);
+            int gemsCount = gems.Length;
+
+            switch (filter)
+            {
+                case "Sum Left":
+                    for (int i = 0; i < gemsCount; i++)
+                    {
+                        int left = (i == 0) ? 0 : gems[i - 1];
+                        if ((left + gems[i]) == arg)
+                        {
+                            indexOfExtracted.Add(i);
+                        };
+                    }
+                    break;
+
+                case "Sum Right":
+                    for (int i = 0; i < gemsCount; i++)
+                    {
+                        int right = (i == gemsCount - 1) ? 0 : gems[i + 1];
+                        if ((right + gems[i]) == arg)
+                        {
+                            indexOfExtracted.Add(i);
+                        };
+                    }
+                    break;
+
+                case "Sum Left Right":
+                    for (int i = 0; i < gemsCount; i++)
+                    {
+                        int left = (i == 0) ? 0 : gems[i - 1];
+                        int right = (i == gemsCount - 1) ? 0 : gems[i + 1];
+                        if ((right + left + gems[i]) == arg)
+                        {
+                            indexOfExtracted.Add(i);
+                        };
+                    }
+                    break;
+            }
+        }
+
     }
 }
