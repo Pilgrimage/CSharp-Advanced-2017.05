39d7af79c9ab3608e7c57b6618a42f9af9c98504
Pilgrimage <nanobot149@gmail.com>
2017-06-18 11:06:12 +0300

    Let's begin with LINQ

---
+++ Ch08_BuiltInQueryMethods_LINQ/lab01_TakeTwo/TakeTwo.cs
@@ -0,0 +1,14 @@
+﻿namespace lab01_TakeTwo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TakeTwo
+    {
+        public static void Main()
+        {
+        }
+    }
+}
